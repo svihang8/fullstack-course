38b4da571f60196c2db580fe013e6ba293d36ff7
Vihang Shah <svihang8@gmail.com>
2022-06-17 19:29:05 -0400

    backend for phonebook ex2.11 - 2.12

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -1,19 +1,22 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Directory from './components/Directory'
 import Form from './components/Form'
 import SearchBar from './components/SearchBar'
+import axios from 'axios'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
-  const [newName, setNewName] = useState('')
+  const [persons, setPersons] = useState([]);
+  const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('')
   const [newFilter, setNewFilter] = useState('');
 
+  useEffect(() => {
+    axios.get('http://localhost:3001/persons')
+    .then((res) => {
+      console.log('phonebook', res.data);
+      setPersons(res.data);
+    });
+  }, []);
 
   const handleName = (e) => {
     setNewName(e.target.value);
